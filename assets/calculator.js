c7c7e78e6013e644227f16529a6b7659a5b69ce6
rssanuraag <s528169@mail.nwmissouri.edu>
2016-10-21 22:39:31 -0500

    Add files via upload

---
+++ assets/calculator.js
@@ -0,0 +1,16 @@
+ function calculate(){
+                var a = document.calculatex.first.value;
+        var b = document.calculatex.second.value;
+        var d = document.calculatex.thrid.value;
+        document.calculatex.result.value= cal(a,b,d);
+    };
+        
+    function cal(a,b,d){}
+        var e = parseInt(b)-parseInt(d);
+        var f = parseInt(a)-parseInt(d);
+        var c = parseInt(e) / parseInt(f);
+        retrun c;
+
+     }
+
+
